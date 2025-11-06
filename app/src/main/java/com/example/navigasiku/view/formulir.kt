0f53c5d9390e780dbe027bf0e4394abcc6bf139f
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-06 14:54:10 +0700

    add submit button with onClick callback

--- app/src/main/java/com/example/navigasiku/view/formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/formulir.kt
@@ -46,6 +46,59 @@ fun FormIsian(
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {}
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = Thickness, color = Color.Red
+            )
 
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                jenisK.forEach { item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = false,
+                            onClick = { /* TODO: handle click and update state */ }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {},
+            )
+
+            Spacer(modifier = Modifier.height(height = 30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f),
+                onClick = onSubmitBtnClick
+            ) {
+                Text(text = stringResource(id = "Submit"))
+            }
+
+        }
     }
 }
