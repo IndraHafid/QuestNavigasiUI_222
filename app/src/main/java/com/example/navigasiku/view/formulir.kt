8c79dec6d9be7f5d3aa9dd2e3a3ee787fec4b05f
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-06 14:47:42 +0700

    setup form layout with scaffold and top app bar

---
+++ app/src/main/java/com/example/navigasiku/view/formulir.kt
@@ -0,0 +1,51 @@
+@file:OptIn(ExperimentalMaterial3Api::class) // Pastikan ini adalah file-level OptIn
+
+package com.example.navigasiku.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitBtnClick: () -> Unit
+) {
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home),
+                    color = Color.White) },
+                colors = TopAppBarDefaults.
+                topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+        }) { isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
+}
