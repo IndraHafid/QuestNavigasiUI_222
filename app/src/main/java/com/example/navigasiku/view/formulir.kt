fc7254e5723eaa5085e8104979e85b4b892cd12e
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-06 15:19:13 +0700

    Tambah input field alamat

--- app/src/main/java/com/example/navigasiku/view/formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/formulir.kt
@@ -74,6 +74,16 @@ fun FormIsian(
                 color = Color.Black
             )
 
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {},
+            )
+
+            Spacer(modifier = Modifier.height(height = 30.dp))
 
         }
     }
